069f0e75edf78bd64c3c1dcc9b8e60a00cf8d3cc
Guilhem Martin <226993+guilhem-martin@users.noreply.github.com>
2024-12-02 21:34:07 +0100

    feat: add basic clt robot

--- Assets/RobotClt.cs
+++ Assets/RobotClt.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class RobotClt : MonoBehaviour
 {
+
+
+    public List<Wheel> wheels = new List<Wheel>();
+
+    Rigidbody rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!rb.isKinematic)
+        {
+            if (Input.GetKey(KeyCode.H))
+            {
+                transform.position += Vector3.up * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.L))
+            {
+                transform.position += Vector3.down * Time.deltaTime;
+            }
+        }
     }
 }
