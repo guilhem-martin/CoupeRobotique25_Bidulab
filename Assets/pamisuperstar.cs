4bab568a566d0ba8f55aec4a593e06f03a96899a
Guilhem Martin <226993+guilhem-martin@users.noreply.github.com>
2024-12-30 21:41:41 +0100

    feat: add superstar

---
+++ Assets/pamisuperstar.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pamisuperstar : MonoBehaviour
+{
+
+    Rigidbody rb;
+
+    // Ficelle booléenne pour déclencher le mouvement
+    private bool isMoving = false;
+
+    public float moveSpeed = 0.01f;
+
+    private float timer = 0.0f;
+
+    // Premier mouvement de l'objet pendant 1.25 secondes
+    private float waitTime1 = 1.95f;
+
+    // Deuxième mouvement de l'objet pendant 2 secondes
+    private float waitTime2 = 0.35f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isMoving)
+        {
+
+            Debug.Log($"isMoving: {isMoving}");
+            Debug.Log($"timer: {timer}");
+            Debug.Log($"waitTime1: {waitTime1}");
+            Debug.Log($"Time.deltaTime: {Time.deltaTime}");
+
+            timer += Time.deltaTime;  // The interval in seconds from the last frame to the current one (Read Only).
+            // D'abord, l'objet se déplace tout droit vers la scène
+
+            if (timer < waitTime1)
+                transform.position += new Vector3(0, 0, moveSpeed);
+            else if (timer < waitTime2 + waitTime1)
+                transform.position += new Vector3(moveSpeed, 0, 0);
+
+        }
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            isMoving = true;
+        }
+    }
+}
