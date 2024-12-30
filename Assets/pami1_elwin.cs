261e31b4c33b27a869fce55f72793af8d81724fd
Guilhem Martin <226993+guilhem-martin@users.noreply.github.com>
2024-12-30 16:28:57 +0100

    chore: add logs

--- Assets/pami1_elwin.cs
+++ Assets/pami1_elwin.cs
@@ -16,6 +16,12 @@ public class pami1_elwin : MonoBehaviour
 
     // Premier mouvement de l'objet pendant 1.25 secondes
     private float waitTime1 = 1.25f;
+    private bool premier_mouvement = true;
+
+    // Deuxième mouvement de l'objet pendant 2 secondes
+    private float waitTime2 = 2.0f;
+    private bool deuxieme_mouvement = true;
+
 
 
     // Start is called before the first frame update
@@ -27,12 +33,26 @@ public class pami1_elwin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (isMoving)
         {
+
+            Debug.Log($"isMoving: {isMoving}");
             timer += Time.deltaTime;  // The interval in seconds from the last frame to the current one (Read Only).
             // D'abord, l'objet se déplace tout droit vers la scène
-            if (timer < waitTime1)
+            if (timer < waitTime1 && premier_mouvement)
                 transform.position += new Vector3(0, 0, moveSpeed);
+            else
+                premier_mouvement = false;
+                timer = 0.0f;
+
+
+            // Ensuite l'objet se déplace en diagonale vers la droite de la scène
+            if (timer < waitTime2 && !premier_mouvement && deuxieme_mouvement)
+                transform.position += new Vector3(moveSpeed, 0, moveSpeed);
+            else
+                deuxieme_mouvement = false;
+                timer = 0.0f;
         }
 
         if (Input.GetKey(KeyCode.F))
