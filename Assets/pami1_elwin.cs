e5fab6a5105b5445f4a5208f9334af888991395e
Guilhem Martin <226993+guilhem-martin@users.noreply.github.com>
2024-12-30 15:43:01 +0100

    feat: variabilize move speed

--- Assets/pami1_elwin.cs
+++ Assets/pami1_elwin.cs
@@ -10,6 +10,8 @@ public class pami1_elwin : MonoBehaviour
     // Ficelle booléenne pour déclencher le mouvement
     private bool isMoving = false;
 
+    public float moveSpeed = 0.01f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +24,9 @@ public class pami1_elwin : MonoBehaviour
     {
         if (isMoving)
         {
-            transform.position += new Vector3(0.01f, 0, 0);
+            // D'abord, l'objet se déplace tout droit vers la scène
+            transform.position += new Vector3(0, 0, moveSpeed);
+            // Il le fait pendant 1.25 secondes
         }
 
         if (Input.GetKey(KeyCode.F))
