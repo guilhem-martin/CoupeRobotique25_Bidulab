98fd08ccc0929d905e172190f345b50e2790fcba
Guilhem Martin <226993+guilhem-martin@users.noreply.github.com>
2025-01-03 13:41:41 +0100

    feat: add roue_pami1 script to rotate wheel and update SampleScene with new GameObjects

---
+++ Assets/roue_pami1.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roue_pami1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // this wheel shows rotation
+        transform.Rotate(0, 0, 10);
+
+    }
+}
