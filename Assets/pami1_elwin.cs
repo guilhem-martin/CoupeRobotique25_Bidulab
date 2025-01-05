9a427003e00ac754cbf013daf6ff443e9f626a98
Guilhem Martin <226993+guilhem-martin@users.noreply.github.com>
2025-01-05 18:01:37 +0100

    feat: update pami1_elwin to rotate object after movement completion

--- Assets/pami1_elwin.cs
+++ Assets/pami1_elwin.cs
@@ -80,12 +80,22 @@ public class pami1_elwin : MonoBehaviour
             else if (timer < waitTime2 + waitTime1)
                 transform.position += new Vector3(moveSpeed, 0, moveSpeed);
             else if (timer < waitTime3 + waitTime2 + waitTime1)
+            {
                 transform.position += new Vector3(-moveSpeed, 0, 0);
-            else
+            }
+
+
+
+
+            if (timer > waitTime3 + waitTime2 + waitTime1)
             {
                 isMoving = false;
                 roueScript1.Move(false);
                 roueScript2.Move(false);
+                float degrees = 90;
+                //Vector3 to = new Vector3(0, degrees, 0);
+                // transform.rotation = Quaternion.Euler(to);
+                transform.Rotate(0, degrees, 0);
             }
         }
 
